6f713ca9958d2dfa3f9f45b84067708aac9d9a8b
Charlie Root <root@DEV.fritz.box>
2022-08-04 15:05:45 +0000

    Initial app

---
+++ header.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+<title> Android App store Analyser </title>
+<link href="css/styles.css" rel="stylesheet">
+</head>
+<body id="main">
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include_once 'header.php'; ?>
+<div class="wrapper">
+	<div class="banner">
+	</div>
+	<div class="content">
+		<div class="table">
+			Hello my name is david
+		</div>
+	</div>
+</div>
+<?php include_once "footer.php"; ?>
