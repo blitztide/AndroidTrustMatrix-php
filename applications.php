ada9faaa3c58b73a972b1de306cfea85c51e6095
User David <david@DEV.fritz.box>
2022-10-20 09:20:17 +0000

    Fixed CSS

--- applications.php
+++ applications.php
@@ -15,16 +15,23 @@ if(!isset($_GET['page'])){
 
 $base = ($page_number - 1) * $limit;
 
-$sql = 'SELECT * from Applications LIMIT ' . $base . ','. $limit . ';';
+$sql = 'SELECT * from Applications ORDER BY PackageName LIMIT ' . $base . ','. $limit . ';';
 
 $result = $conn->query($sql);
 
+$content .= '<div class="expanding">';
+
 if ($result->num_rows > 0)
 {
-	$content .= '<li><table class="list"><tr><th>Application Name</th><th>Version</th><th>MD5</th><th>is Malware</th></tr>';
+	$content .= '<li><table class="list"><tr><th>Application Name</th><th>Version</th><th>MD5</th></tr>';
 	while ($row = $result->fetch_assoc())
 	{
-		$content .= '<tr><td>' . $row['PackageName'] . '</td><td>'. $row['Version'].'</td><td>'. $row['MD5Sum'] .'</td><td>' . $row['IsMalware'] . '</td></tr>';
+		if ($row['IsMalware'] == True){
+			$content .= '<tr style="color:red"><td>' . $row['PackageName'] . '</td><td>'. $row['Version'].'</td><td>'. $row['MD5Sum'] .'</td><td>' . '</td></tr>';
+		}
+		else {
+			$content .= '<tr><td>' . $row['PackageName'] . '</td><td>'. $row['Version'].'</td><td>'. $row['MD5Sum'] .'</td></tr>';
+		}
 	}
 
 } else {
@@ -32,6 +39,7 @@ if ($result->num_rows > 0)
 }
 $conn -> close();
 $content .= '</table></ul>';
+$content .= '</div>';
 $content .= '</li><li><a href="/applications.php?page=' . ($page_number - 1) . '">Previous page</a><a href="/applications.php?page=' . ($page_number + 1 ) .'">Next page</a></li>';
 drawpage($content);
 ?>
